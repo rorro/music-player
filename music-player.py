679d99e3d0a227e3067ac515432851924bc0c0ee
Robin Sliwa <rorrosliwa@gmail.com>
2020-05-18 14:16:30 +0200

    Run server on local ip

--- music-player.py
+++ music-player.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from home.views import bp
 
+import socket
+
 def create_app(config_file):
     app = Flask(__name__)
     app.config.from_pyfile(config_file)
@@ -8,6 +10,17 @@ def create_app(config_file):
 
     return app
 
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('10.255.255.255', 1))
+        ip = s.getsockname()[0]
+    except:
+        ip = '127.0.0.1'
+    finally:
+        s.close()
+    return ip
+
 if __name__ == '__main__':
     app = create_app('settingslocal.py')
-    app.run()
+    app.run(host=get_ip(), port='5000')
